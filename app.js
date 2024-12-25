1069ea8bc593bf98e13f3fa65b1d62504662090f
RezaAzharii <rezaazhari867@gmail.com>
2024-12-25 21:25:24 +0700

    nambah navbar

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const session = require("express-session");
 
 app.use(expressLayouts);
 app.use(express.json());
-
+app.use(express.static('public'))
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
