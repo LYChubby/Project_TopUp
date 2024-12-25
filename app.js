648e6ef12a66886251e4b2cb388eb65e5fa0fa03
LYChubby <f4uzi345@gmail.com>
2024-12-25 21:10:22 +0700

    Start project

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+// const todoRoutes = require("./routes/tododb.js");
+require("dotenv").config();
+const port = process.env.PORT;
+const db = require("./database/db");
+const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+// const authRoutes = require("./routes/authRoutes");
+
+app.use(expressLayouts);
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your_secret_key", // Gunakan secret key yang aman
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }, // Set ke true jika menggunakan HTTPS
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    layout: "layouts/main-layout",
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
